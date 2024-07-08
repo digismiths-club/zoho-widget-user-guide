5d1fb1f6a48429e2a94f44d32ee27ac08a4fdd5e
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-07-08 15:55:24 +0530

    footer updated

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
   return (
     <div className='mt-14 bg-[#213346] h-14 flex items-center justify-center text-white py-7 w-full space-x-1'>
-      <span>Made with love by Team </span>
-      <a href="https://digismiths.com/" target="_blank" rel="noopener noreferrer" className=''>Digismiths ❤️</a>
+      <span>Made with love by </span>
+      <a href="https://digismiths.com/" target="_blank" rel="noopener noreferrer" className='text-[#ff0000] hover:text-[#991e1e]'>Team Digismiths ❤️</a>
     </div>
   )
 }
