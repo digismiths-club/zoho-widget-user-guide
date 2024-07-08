d4f2329b824c64640eac485eb280b46e07a9ca32
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-07-08 15:47:36 +0530

    footer updated

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='mt-14 bg-[#213346] h-14 flex items-center justify-center text-white py-7 w-full '>
-      Made with love by Team Digismiths ❤️
+    <div className='mt-14 bg-[#213346] h-14 flex items-center justify-center text-white py-7 w-full space-x-1'>
+      <span>Made with love by Team </span>
+      <a href="https://digismiths.com/" target="_blank" rel="noopener noreferrer" className=''>Digismiths ❤️</a>
     </div>
   )
 }
