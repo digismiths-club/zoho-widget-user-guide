e7b32ac988620cca3d450bb13117b6f3db0d2484
Puneet Chandhok <punu13@gmail.com>
2025-06-09 15:48:07 +0530

    removed free reference

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -9,7 +9,7 @@ function  About() {
           <div className='xl:text-4xl xl:mt-2 font-semibold xl:font-normal'>Google Maps Autocomplete</div>
           <div className='xl:mt-2 mt-1 text-sm xl:text-lg'>Premium Autocomplete & Address Formatter by <b>Digismiths</b></div>
           <div className='xl:mt-4 mt-2 font-light text-xs xl:text-base'>
-            Simplify your Zoho CRM with our free widget that integrates Google Maps Autocomplete for faster, more accurate address entry. No more manual input or address errors—just seamless mapping and improved data quality. Perfect for businesses looking to save time and enhance workflow efficiency! 
+            Simplify your Zoho CRM with our premium widget that integrates Google Maps Autocomplete for faster, more accurate address entry. No more manual input or address errors—just seamless mapping and improved data quality. Perfect for businesses looking to save time and enhance workflow efficiency! 
           </div>
           <a href="https://marketplace.zoho.com/app/crm/google-address-autocomplete-widget-for-zoho-crm" target="_blank">
             <button className='mt-7 bg-blue-400 py-2 px-3 rounded-md text-white hover:bg-blue-500 text-sm'>
